d75733d5250df78e54afc812600593abcfd4c568
designpigofficial <designpig@outlook.com>
2022-08-16 17:24:45 -0600

    ci: setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
-/** @type {import('tailwindcss').Config} */ 
 module.exports = {
     content: [
       "./pages/**/*.{js,ts,jsx,tsx}",
       "./components/**/*.{js,ts,jsx,tsx}",
     ],
+    darkMode: 'class', // or 'media' or 'class'
     theme: {
+      extend: {
+        backgroundVideo: {
+          'hero-section': "url('/homepage/robovice-banner.mp4')",
+        }
+      },
+    },
+    variants: {
       extend: {},
     },
     plugins: [],
   }
+
